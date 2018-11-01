f3244b2dfd7578f092401432c4569dcd5a2edc85
Toni <arodriguezt@gmail.com>
2018-11-01 08:56:38 +0100

    Fist function third exercise

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -11,7 +11,7 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    namedExpressions.transform((key, value) => Signal(eval(value(), namedExpressions)))
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
